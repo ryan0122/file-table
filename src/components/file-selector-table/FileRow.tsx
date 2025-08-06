e89fc426f42b3b10901b5e8773cdbca1590c1eaa
ryan0122 <your@email.com>
2025-08-06 12:02:02 -0600

    clean up css styles, enhance tr to be clickable/selectable

--- src/components/file-selector-table/FileRow.tsx
+++ src/components/file-selector-table/FileRow.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useId} from "react";
 import { File } from "./data";
 import styles from "./styles.module.css";
 
@@ -13,6 +13,8 @@ export const FileRow = ({file, isChecked, onToggle}: FileRowProps) => {
 	// Requirements are unclear, but assuming we shouldn't let the user select files that are not available to download
 	const isSelectable = file.status === 'available';
 	const checkboxSrText = isSelectable ? `Select file ${file.name} on device ${file.device}` : `File ${file.name} on device ${file.device} is not available for selection`;
+
+	const checkboxId = useId();
 	
 	// Define the class names for the status cell based on the file's status
 	const statusClasses = [
@@ -23,16 +25,17 @@ export const FileRow = ({file, isChecked, onToggle}: FileRowProps) => {
 
 	return (
 		// Requirements do no indicate that rows should be clickable, so will rely on the checkbox for selection
-		<tr className={isChecked ? styles.selected : undefined}>
+		<tr className={`${isChecked ? styles.selected : ''} ${isSelectable ? styles.selectable : ''}`}>
 			<td>
 				<input 
 					type="checkbox" 
 					checked={isChecked}
 					onChange={() => onToggle()}
-					aria-label={checkboxSrText}
 					disabled={!isSelectable}
 					name={file.name}
+					id={checkboxId}
 				/>
+				<label htmlFor={checkboxId}><span className={styles.srOnly}>{checkboxSrText}</span></label>
 			</td>
 			<td>{file.name}</td>
 			<td>{file.device}</td>
